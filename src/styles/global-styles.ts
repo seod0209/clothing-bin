a8418e85faa41d222cb8cb0d22f8abf18392aebb
seod0209 <don9zseo@gmail.com>
2024-03-20 01:00:07 +0900

    feat: add global style component and style theme, prettierrc.js

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *, *:: after, *::before {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html, body {
+        margin: 0;
+        width: 100%;
+        height: 100%;
+        background-color: ${({ theme }) => theme.colors.background.primary};
+        color: ${({ theme }) => theme.colors.text.primary};
+        font-size: 16px;
+
+        /**
+         * iOS Safari doesn't respect the user-scalable=no meta tag since iOS 10
+         * https://stackoverflow.com/questions/65100185/how-to-disable-all-zoom-on-mobile-safari
+         */
+        touch-action: pan-x pan-y;
+
+        @media screen and (max-width: 768px) {
+        min-width: 360px;
+        }
+    }
+
+    ol, ul{
+    list-style: none;
+    }
+
+    input, button {
+        background-color: transparent;
+        border: none;
+        outline: none;
+        cursor: pointer;
+        font-family: Pretendard-Regular;
+    }
+
+    p, span {
+        margin: 0;
+        padding: 0;
+    }
+
+    @font-face {
+        font-family: 'Pretendard-Regular';
+        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+        font-weight: 400;
+        font-style: normal;
+    }
+`;
