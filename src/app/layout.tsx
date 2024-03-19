8d3e908eed4f0aef5853d7edec2cc478b3255757
seod0209 <don9zseo@gmail.com>
2024-03-19 17:18:38 +0900

    style: apply global style on root layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,32 @@
 import type { Metadata } from "next";
 
+import StyledComponentRegistry from "@/lib/styled-components-registry";
+import { Pretendard } from "@/styles/fonts";
+
+export const metadata: Metadata = {
+  title: "옷체통",
+  description: "서울시 헌옷 수거함 위치 정보 표시",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  },
+  themeColor: "#ffffff",
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ko">
+      <body className={`${Pretendard.className}`}>
+        <StyledComponentRegistry>
+          <main>{children}</main>
+        </StyledComponentRegistry>
+      </body>
     </html>
   );
 }
