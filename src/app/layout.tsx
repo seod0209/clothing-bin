099a26822219327a2e0c2d17cbfafff087b451a7
seod0209 <don9zseo@gmail.com>
2024-03-19 17:58:42 +0900

    feat: edit generate viewport in layout, add 'use client' to using client component - https://nextjs.org/docs/app/api-reference/functions/generate-viewport - https://nextjs.org/docs/messages/context-in-server-component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 import StyledComponentRegistry from "@/lib/styled-components-registry";
 import { Pretendard } from "@/styles/fonts";
@@ -6,13 +6,14 @@ import { Pretendard } from "@/styles/fonts";
 export const metadata: Metadata = {
   title: "옷체통",
   description: "서울시 헌옷 수거함 위치 정보 표시",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
-  },
-  themeColor: "#ffffff",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  themeColor: "white",
 };
 
 export default function RootLayout({
---
+++ src/components/common/text/HeaderText.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const HeaderText = styled.h1`
+  color: green;
+`;
+
+export default HeaderText;
