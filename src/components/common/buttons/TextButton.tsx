19c32ea9337b9b1851e11f218b3a204bb372dc0e
seod0209 <don9zseo@gmail.com>
2024-03-19 22:33:52 +0900

    feat: add GNB - add text button - add icon - add GNB header
    chore: edit files and folder name
    - header to GNB

---
+++ src/components/common/buttons/TextButton.tsx
@@ -0,0 +1,34 @@
+import { FC, type ButtonHTMLAttributes } from "react";
+
+import styled from "styled-components";
+
+const TextButtonContainer = styled.button`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+
+  padding: 8px;
+  width: 100%;
+  height: 40px;
+
+  &:hover {
+    text-decoration: underline;
+    text-underline-offset: 2px;
+    text-underline-position: under;
+    text-decoration-color: #00000033;
+  }
+  &:active {
+    text-decoration: underline;
+    text-underline-position: under;
+    text-underline-offset: 2px;
+    text-decoration-color: black;
+  }
+`;
+
+const TextButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
+  children = undefined,
+  ...props
+}) => <TextButtonContainer {...props}>{children}</TextButtonContainer>;
+
+export default TextButton;
--- src/components/common/text/HeaderText.jsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const HeaderText = styled.h1`
-  color: green;
-`;
-
-export default HeaderText;
